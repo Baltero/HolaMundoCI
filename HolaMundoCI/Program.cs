9d193e4161c6870ef8cddb433a559c556e941221
Baltero <ro8ber8son8@gmail.com>
2025-06-13 23:53:39 -0400

    Primer commit: Hola Mundo CI

---
+++ HolaMundoCI/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace HolaMundoCI
+{
+class program
+    { static void main(string[] args)
+        {
+            Console.WriteLine("Hola mundo desde integracion continua con Gihub Actions");
+        }
+    }
+   }
